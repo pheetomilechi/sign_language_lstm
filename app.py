2490b737e92ef579c1d36345746d18c3af180eb9
pheetomilechi <tomilechighajiga@gmail.com>
2026-09-08 20:41:31 +0100

    Add Streamlit Cloud app entrypoint

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.join(ROOT_DIR, "sign_language_lstm")
+
+if APP_DIR not in sys.path:
+    sys.path.insert(0, APP_DIR)
+
+import streamlit_app
+
+
+if __name__ == "__main__":
+    streamlit_app.main()
